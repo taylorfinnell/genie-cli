2d4464b1b5b7386461beb80051357c7b1bccd4ca
Taylor Finnell <tmfinnell@gmail.com>
2017-07-18 17:26:31 -0400

    consolidate fixtures

--- spec/genie/client_spec.cr
+++ spec/genie/client_spec.cr
@@ -3,23 +3,9 @@ require "../spec_helper"
 module Genie::Model
   @@config : Config = Config.new
 
-  @@job : String = {
-    "id"        => "123",
-    "status"    => "RUNNING",
-    "started"   => "2017-05-01T19:56:53Z",
-    "finished"  => "2017-05-02T19:56:53Z",
-    "name"      => "blah",
-    "outputURI" => "uri",
-  }.to_json
+  @@job : String = JOB_ATTRS.to_json
 
-  @@jobs : String = [{
-    "id"        => "123",
-    "status"    => "RUNNING",
-    "started"   => "2017-05-01T19:56:53Z",
-    "finished"  => "2017-05-02T19:56:53Z",
-    "name"      => "blah",
-    "outputURI" => "uri",
-  }].to_json
+  @@jobs : String = [JOB_ATTRS].to_json
 
   describe Client do
     Spec.before_each { WebMock.reset }
--- spec/genie/model/job_spec.cr
+++ spec/genie/model/job_spec.cr
@@ -4,14 +4,7 @@ module Genie::Model
   describe Job do
     describe "from_json" do
       it "uses local tz" do
-        json = {
-          "id"        => "123",
-          "status"    => "RUNNING",
-          "started"   => "2017-05-01T19:56:53Z",
-          "finished"  => "2017-05-02T19:56:53Z",
-          "name"      => "blah",
-          "outputURI" => "uri",
-        }.to_json
+        json = JOB_ATTRS.to_json
 
         job = Job.from_json(json)
         with_timezone("America/New_York") do
